e310b24bbbbcbb76d5c96622f5030b9391d050de
Sumia Hago <sumia.hago15@myhunter.cuny.edu>
2023-02-06 17:01:44 -0500

    gitingnore working

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function changeColor() {
+	let color = document.body.style.color;
+	if (color === 'white') {
+		document.body.style.color = 'black';
+	} else {
+		document.body.style.color = 'white';
+	}
+}
